524fc0b49e3687129776194f09c281c4c76d4a41
Paulopez13 <pau.lopez.miras@gmail.com>
2021-11-10 14:46:42 +0100

    20211110 trabajos clase2

--- uf1nf1/BARAJACATALANA/src/barajacatalana/BARAJACATALANA.java
+++ uf1nf1/BARAJACATALANA/src/barajacatalana/BARAJACATALANA.java
@@ -13,7 +13,7 @@ import java.util.Scanner;
  */
 public class BARAJACATALANA {
 
-    /**
+    /** hola
      * @param args the command line arguments
      */
     public static void main(String[] args) {
